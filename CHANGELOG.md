bae4fb6c949c3a6816c7c011e19d04a62dc7f1ca
guochao <463561248@qq.com>
2022-02-22 10:51:53 +0800

    📦️ build:  发布0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # @dangojs/vue3-transitions
 
+## 0.0.2
+
+### Patch Changes
+
+- 增加 SlideYUpTransition 组件
+-
+
 ## 0.0.1
 
 ### Patch Changes
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 /*
  * @Author: saber
  * @Date: 2022-02-18 10:12:22
- * @LastEditTime: 2022-02-22 10:40:28
+ * @LastEditTime: 2022-02-22 10:47:18
  * @LastEditors: saber
  * @Description: 
  */
 // TODO: 后面会添加 install 方法
 import FadeTransition from './Fade/FadeTransition.vue';
 import SlideYUpTransition from './Slide/SlideYUpTransition.vue';
-
-// export default _Pagination;
-console.log('test', FadeTransition.name)
 
 export {
     FadeTransition,
